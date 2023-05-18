8d08ec1dd33ba7f075594582c3dced435583c608
Danielpatinno <Neniade777@gmail.com>
2023-05-18 07:51:17 -0300

    ex006 concluído.

--- ex006.js
+++ ex006.js
@@ -1,1 +1,26 @@
-// 
+// Escreva uma função que receba três números como parâmetros e retirne a soma dos três
+
+
+function somarNumeros(n1,n2,n3) {
+    
+    return n1 + n2 + n3;
+}
+
+// console.log(somarNumeros(2,5,4));
+
+// Somar 3 números que estão dentro de um array
+
+const numeros = [2,5,4]
+
+function somarArray(numeros) {
+
+    let resultado = 0;
+
+    numeros.map((n) => {
+        resultado += n;
+    })
+
+    return resultado;
+}
+
+console.log(somarArray(numeros))
